9fe92239a2ee7c6d9f6070f0283f85ef74a4e0da
Toshtonov <shakhtoshtonov@gmail.comu@gmail.com>
2025-02-11 14:53:36 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("openModal");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+}
